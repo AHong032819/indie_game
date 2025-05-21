3f129433c688dab6f22100b99d58b221353bccc2
AHongNB <pingho1119@gmail.com>
2025-05-21 11:42:20 +0800

    完善腳色動畫

--- Assets/code/PlayerMovement.cs
+++ Assets/code/PlayerMovement.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+
+    public Animator animator;
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
@@ -17,10 +19,18 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
