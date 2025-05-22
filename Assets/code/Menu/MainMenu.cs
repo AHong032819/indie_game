b46568553d778a18955a697838b9fc479bbd991a
AHongNB <pingho1119@gmail.com>
2025-05-22 09:34:06 +0800

    新增菜單與暫停選單

--- Assets/code/Menu/MainMenu.cs
+++ Assets/code/Menu/MainMenu.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,5 +8,9 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
