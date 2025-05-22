ae0b81a55f057b976838222765c3315302425f70
AHongNB <pingho1119@gmail.com>
2025-05-22 17:17:19 +0800

    一堆東西

--- Assets/code/Player/Health-Bar-master/Health Bar/Assets/Player.cs
+++ Assets/code/Player/Health-Bar-master/Health Bar/Assets/Player.cs
@@ -6,8 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-	[SerializeField]GameObject deathmenu;
-	[SerializeField]GameObject pausebutton;
+	[SerializeField] GameObject deathmenu;
+	[SerializeField] GameObject pausebutton;
+	[SerializeField] GameObject Apple;
+	[SerializeField] GameObject banana;
+
 	public int maxHealth = 5;
 	public int currentHealth;
 
@@ -31,18 +34,46 @@ public class Player : MonoBehaviour
 		{
 			TakeDamage(5);
 		}
+		if (other.gameObject.tag == "Saw")
+		{
+			TakeDamage(3);
+		}
 		if (currentHealth <= 0)
 		{
 			Time.timeScale = 0;
 			deathmenu.SetActive(true);
 			pausebutton.SetActive(false);
 		}
-		
-	
+		if (other.gameObject.tag == "Apple")
+		{
+			AddDealth(1);
+			Apple.SetActive(false);
+		}
+		if (other.gameObject.tag == "banana")
+		{
+			AddDealth(1);
+			banana.SetActive(false);
+		}
+		if (other.gameObject.tag == "Finish flag")
+		{
+			SceneManager.LoadSceneAsync("level2");
+		}
+		if (other.gameObject.tag == "Finish flag2")
+		{
+			SceneManager.LoadSceneAsync("level1");
+		}
+
+
 	}
 	void TakeDamage(int damage)
 	{
 		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
+	void AddDealth(int Health)
+	{
+		currentHealth += Health;
 
 		healthBar.SetHealth(currentHealth);
 	}
