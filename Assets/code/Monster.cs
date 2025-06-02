54db11a2f7a0476d080188f201cf38a7c3e2abd6
onlyfanuser <119887abc@gmail.com>
2025-06-02 12:04:34 +0800

    敵人巡邏

---
+++ Assets/code/Monster.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SimplePatrol : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 2f;
+
+    private Vector3 target;
+
+    void Start()
+    {
+        target = pointB.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target) < 0.05f)
+        {
+            if (target == pointA.position)
+                target = pointB.position;
+            else
+                target = pointA.position;
+
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+       Vector3 scale = transform.localScale;
+        scale.x *=-1;
+        transform.localScale = scale;
+    }
+}
